1ff465b7a890d00daaf76d276e2765bdd62cfde2
Olamide Jon-Ajumobi <olamideajumobi@gmail.com>
2024-06-20 01:38:41 +0100

    Starting-Project

--- ApplicationPortal.API/Program.cs
+++ ApplicationPortal.API/Program.cs
@@ -10,14 +10,16 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
+app.UseRouting();
 
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("./swagger/v1/swagger.json", "Application Portal API");
+    c.RoutePrefix = "";
+});
 app.UseAuthorization();
 
 app.MapControllers();
